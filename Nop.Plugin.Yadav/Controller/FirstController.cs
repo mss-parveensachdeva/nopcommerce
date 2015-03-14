3b9da27f7a9d32b220a1ff3938bca866bb2efd7f
Vimal Sharma <mss.vimalsharma@gmail.com>
2015-03-14 15:54:20 +0530

    Initial Commit

---
+++ Nop.Plugin.Yadav/Controller/FirstController.cs
@@ -0,0 +1,14 @@
+﻿using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nop.Plugin.Yadav.Controller
+{
+    public class FirstController
+    {
+       // public ActionResult  
+
+    }
+}
---
+++ Nop.Plugin.Yadav/Yadav.cs
@@ -0,0 +1,34 @@
+﻿using Nop.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nop.Plugin.Yadav
+{
+    public class Yadav : IPlugin 
+    {
+
+        public PluginDescriptor PluginDescriptor
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
